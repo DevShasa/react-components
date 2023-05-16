a6d8f1cad700c832bc6477adfb0b90d71717602c
DevShasa <shasa.thuo@gmail.com>
2023-05-16 06:53:48 +0300

    extract comments

--- nested_comment_system/client/src/contexts/PostContext.js
+++ nested_comment_system/client/src/contexts/PostContext.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useMemo} from "react"
 import { useAsync } from "../hooks/useAsync"
 import { getPost } from "../services/posts"
 import { useParams } from "react-router-dom"
@@ -9,7 +9,19 @@ export const PostProvider=({children})=>{
 
     const { id } = useParams()
     const {loading, error, value:post} =useAsync(()=>getPost(id), [id])
+    const [comments, setComments] = useState([])
 
+    const commentsByParentId = useMemo(()=>{
+        const group = {}
+        comments.forEach(comment=>{
+            // extract the comments with the parent id
+            group[comment.parentId] ||= []
+            group[comment.parentId].push(comment)
+            
+        })
+
+        return group
+    }, [comments])
 
     console.log(post)
     return (<Context.Provider value={{}}>
