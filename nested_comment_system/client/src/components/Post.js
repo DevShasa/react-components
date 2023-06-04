e2d78cbd94a255d3301d550861d1c6e9528321f7
DevShasa <shasa.thuo@gmail.com>
2023-06-04 09:46:14 +0300

    post page and comments

--- nested_comment_system/client/src/components/Post.js
+++ nested_comment_system/client/src/components/Post.js
@@ -1,5 +1,22 @@
+import { usePost } from "../contexts/PostContext"
+import Commentlist from "./Commentlist"
+
+
 export function Post(){
-    return (<div>
-        Hi
-    </div>)
+
+    const { post, rootComments } = usePost()
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <article>{post.body}</article>
+            <h3 className="comments-title">Comments</h3>
+            <section>
+                {rootComments !== null && rootComments.length !== 0  &&(
+                    <div className="mt-4">
+                        <Commentlist comments={rootComments}/>
+                    </div>
+                )}
+            </section>
+        </>
+    )
 }
--- nested_comment_system/client/src/contexts/PostContext.js
+++ nested_comment_system/client/src/contexts/PostContext.js
@@ -1,9 +1,13 @@
-import React, {useState, useMemo} from "react"
+import React, {useState, useMemo, useContext, useEffect} from "react"
 import { useAsync } from "../hooks/useAsync"
 import { getPost } from "../services/posts"
 import { useParams } from "react-router-dom"
 
 const Context = React.createContext()
+
+export function usePost(){
+    return useContext(Context)
+}
 
 export const PostProvider=({children})=>{
 
@@ -13,22 +17,30 @@ export const PostProvider=({children})=>{
 
     const commentsByParentId = useMemo(()=>{
         const group = {}
-        comments.forEach(comment=>{
+        comments?.forEach(comment=>{
             // extract the comments with the parent id
             // group[comment.parentId] ||= []
             // group[comment.parentId].push(comment)
-            
+
             if(group[comment.parentId]){
                 // the parent id key exists, add all comments for that parent id 
                 group[comment.parentId].push(comment)
             } else{
                 // parent id key does not existt lets add it
                 group[comment.parentId] = []
+                group[comment.parentId].push(comment)
             }
+
         })
 
         return group
     }, [comments])
+
+    useEffect(()=>{
+        if(post?.comments === null) return
+        setComments(post?.comments)
+    },[post?.comments])
+    
 
     function getRepliesToComment(parentId){
         return commentsByParentId[parentId]
@@ -36,7 +48,7 @@ export const PostProvider=({children})=>{
 
     function createLocalComment(comment){
         setComments(prev=>{
-            return [...comments, prev]
+            return [...comment, prev]
         })
     }
 
@@ -72,10 +84,10 @@ export const PostProvider=({children})=>{
     }
 
 
-    console.log(post)
+    console.log("COMMENTS AND PARENTS",commentsByParentId)
     return (
     <Context.Provider value={{
-        post: {id, ...post},
+        post: {id, ...post}, // adding the id because in the server we dont send it 
         rootComments: commentsByParentId[null],
         getRepliesToComment,
         createLocalComment,
