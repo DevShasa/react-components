3e8a5c4b149e7478181ee5f8e30b44c560282777
DevShasa <shasa.thuo@gmail.com>
2023-07-09 17:45:25 +0300

    Added form component

---
+++ forms/src/components/FormInput.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+const FormInput = (props) => {
+
+    const [focused, setFocused] = useState(false)
+    const {label, onChange,errorMsg, id, ...inputProps} = props
+
+
+    const handleFocus = ()=>{
+        setFocused(true)
+    }
+    return (
+        <div className='formInput'>
+            <label>{label}</label>
+            <input 
+                {...inputProps} 
+                onChange = {onChange} 
+                required  
+                onBlur={handleFocus}  // activates when user clicks away from element 
+                customAtributeToSupportShowErrorLogic={focused.toString()}
+                onFocus={()=>inputProps.name === "confirmPassword" && setFocused(true)} // activates when user clicks element
+            />
+            <span>{errorMsg}</span>
+        </div>
+    )
+}
+
+export default FormInput
