4048d68a78a4fff4eac6beff8ecc9afa8af67f6b
DevShasa <shasa.thuo@gmail.com>
2024-01-30 18:14:40 +0300

    feat: fixed wait bug

--- skeleton_loading_components_from_scratch/src/components/Nav.tsx
+++ skeleton_loading_components_from_scratch/src/components/Nav.tsx
@@ -1,6 +1,5 @@
 import useSWR from "swr";
 import { getUsers } from "../api/usersApi";
-import { usersEdnpoint } from "../utils";
 import { User } from "../globaltypes";
 
 type INavProps = {
@@ -12,7 +11,17 @@ type INavProps = {
 const Nav = (props: INavProps) => {
 	const { currentUserId, setCurrentUserId } = props;
 
-	const { isLoading, error, data: employees, } = useSWR(usersEdnpoint, getUsers);
+
+
+    // async function x(){
+    //     const data = await fetch(`${baseUrl}${usersEdnpoint}`)
+    //     const res = await data.json()
+    //     return res
+    // }
+
+	const { isLoading, error, data: employees, } = useSWR("/users", getUsers);
+    //console.log(`${baseUrl}${usersEdnpoint}`)
+    console.log("---->",employees)
 
 	let options;
 	if (isLoading) {
