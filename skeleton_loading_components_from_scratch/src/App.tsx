925849668ecab6bd4cee01aa28337532ca06533a
DevShasa <shasa.thuo@gmail.com>
2024-03-31 14:40:28 +0300

    chore, set up basic set up for blog article on skeleton components

--- skeleton_loading_components_from_scratch/src/App.tsx
+++ skeleton_loading_components_from_scratch/src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import PostList from './components/PostList'
+// import { useState } from 'react'
+// import Header from './components/Header'
+// import PostList from './components/PostList'
 
-function App() {
-  const [currentUserId, setCurrentUserId] = useState(0)
-  return (
-    <>
-      <Header currentUserId={currentUserId} setCurrentUserId={setCurrentUserId}/>
-      <PostList currentUserId={currentUserId} />
-    </>
-  )
+// function App() {
+//   const [currentUserId, setCurrentUserId] = useState(0)
+//   return (
+//     <>
+//       <Header currentUserId={currentUserId} setCurrentUserId={setCurrentUserId}/>
+//       <PostList currentUserId={currentUserId} />
+//     </>
+//   )
+// }
+
+
+
+import Blog from "./Blog";
+
+function App(){
+  return <Blog />
 }
 
 export default App
--- skeleton_loading_components_from_scratch/src/Blog.tsx
+++ skeleton_loading_components_from_scratch/src/Blog.tsx
@@ -1,1 +1,64 @@
 // code to demonstrate creating skeleton components for my blog
+// Api call that calls ten posts 
+// a delay function that delaus for specified seconds
+// a component that calls the posts usin useeffect
+// useEffect is triggered when reload button is clicked triggering a refetch 
+import { useState, useEffect, useCallback } from "react"
+
+// delay for one second
+const delay = () => new Promise<void>((res)=> setTimeout(()=> res(), 1000))
+
+// api calls for post
+const fetchPosts = async():Promise<[]>=>{
+    await delay()
+    const response = await fetch(`http://localhost:3500/posts`)
+    const data = await response.json()
+    return data
+}
+
+const Blog = () => {
+
+    const [loading, setLoading ] = useState(false)
+    const [data, setData] = useState([])
+    const [error, setError] = useState("")
+
+
+    console.log("HERE IS THE DATA:::", data)
+
+    const fetchData = useCallback(async()=>{
+        setLoading(true)
+        try {
+            const data = await fetchPosts() 
+            setData(data)
+        } catch (error) {
+            console.log("AN ERROR HAS OCCURED", error)
+            setError("An error has occcured")
+        }finally{
+            setLoading(false)
+        }
+    },[])
+
+    useEffect(()=>{
+        fetchData()
+    },[fetchData])
+
+    
+    let content
+    
+    if(loading){
+        content = <div>Loading</div>
+    }else if(error){
+        content = <div>{`${error}`}</div>
+    }else{
+        content = (
+            <div>
+                kuna content
+            </div>
+        )
+    }
+
+  return content
+
+}
+
+export default Blog
